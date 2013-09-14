c70488e246346104a793697875fdc6d5598fdcd6
Jeremy Kahn <jeremyckahn@gmail.com>
2013-09-14 11:30:47 -0500

    Inits handles.

--- src/bezierizer.core.js
+++ src/bezierizer.core.js
@@ -1,4 +1,15 @@
 function Bezierizer (container) {
-  this.$el  = $(container);
+  this.$el = $(container);
   this.$el.append($(HTML_TEMPLATE));
+
+  this.$canvasContainer = this.$el.find('.bezierizer-canvas-container');
+  this.$canvas = this.$canvasContainer.find('canvas');
+  this.$handles = this.$canvasContainer.find('.bezierizer-handle');
+  this.$readout = this.$canvasContainer.find('.bezierizer-coord-readout');
+
+  this.$canvas[0].height = this.$canvas.height();
+  this.$canvas[0].width = this.$canvas.width();
+  this.$handles.dragon({
+    within: this.$canvasContainer
+  });
 }
--- src/bezierizer.template.js
+++ src/bezierizer.template.js
@@ -1,6 +1,10 @@
 var HTML_TEMPLATE = [
   '<div class="bezierizer-container">'
-    ,'<canvas>'
-    ,'<span class="bezierizer-coord-output"></span>'
+    ,'<div class="bezierizer-canvas-container">'
+      ,'<canvas></canvas>'
+      ,'<button class="bezierizer-handle bezierizer-handle-1">'
+      ,'<button class="bezierizer-handle bezierizer-handle-2">'
+    ,'</div>'
+    ,'<span class="bezierizer-coord-readout"></span>'
   ,'</div>'
 ].join('');
