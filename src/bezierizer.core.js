1192d950f13515d115dc5336fdd8989a00e4d474
Jeremy Kahn <jeremyckahn@gmail.com>
2013-09-15 11:18:35 -0500

    Starts on handle coordinate calculation.

--- src/bezierizer.core.js
+++ src/bezierizer.core.js
@@ -1,3 +1,10 @@
+function getHandleX (bezierizer, $handle) {
+  var handleX = parseInt($handle.css('left'), 10);
+  return handleX / (
+      bezierizer.$canvasContainer.width() - $handle.outerWidth(true));
+}
+
+
 function Bezierizer (container) {
   this.$el = $(container);
   this.$el.append($(HTML_TEMPLATE));
@@ -12,4 +19,16 @@ function Bezierizer (container) {
   this.$handles.dragon({
     within: this.$canvasContainer
   });
+
+  this.$canvasContainer.on('drag', '.bezierizer-handle', $.proxy(function (evt) {
+    console.log(this.getHandlePositions())
+  }, this));
 }
+
+
+Bezierizer.prototype.getHandlePositions = function () {
+  return {
+    x1: getHandleX(this, this.$handles.eq(0))
+    ,x2: getHandleX(this, this.$handles.eq(1))
+  };
+};
