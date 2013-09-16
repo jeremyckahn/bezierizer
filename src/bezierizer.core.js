c4bb0775e244ae5d9971d31272bd6cb85bcf74d6
Jeremy Kahn <jeremyckahn@gmail.com>
2013-09-16 10:41:15 -0500

    Some refactoring, and implementes setHandlePositions.

--- src/bezierizer.core.js
+++ src/bezierizer.core.js
@@ -35,37 +35,53 @@ function Bezierizer (container) {
   this._$canvas = this._$canvasContainer.find('canvas');
   this._$handles = this._$canvasContainer.find('.bezierizer-handle');
 
+  var width = this._$canvas.width();
+  var height = this._$canvas.height();
   this._ctx = this._$canvas[0].getContext('2d');
   var ctx = this._ctx;
   ctx.lineWidth = 3;
   ctx.strokeStyle = '#f0f';
+  this._$canvas[0].height = height;
+  this._$canvas[0].width = width;
 
-  this._$canvas[0].height = this._$canvas.height();
-  this._$canvas[0].width = this._$canvas.width();
   this._$handles.dragon({
     within: this._$canvasContainer
   });
+  this._$handles.eq(0).css({
+    left: 0
+    ,top: height * 0.5
+  });
+  this._$handles.eq(1).css({
+    left: width
+    ,top: height * 0.5
+  });
 
-  this.initBindings();
+  this._initBindings();
+  this._updateInternalStateFromDOM();
+  this.redraw();
 }
 
 
-Bezierizer.prototype.initBindings = function () {
+/*!
+ * @private
+ */
+Bezierizer.prototype._initBindings = function () {
   this._$canvasContainer.on(
       'drag', '.bezierizer-handle', $.proxy(function (evt) {
+    this._updateInternalStateFromDOM();
     this.redraw();
     this.$el.trigger('change');
   }, this));
 };
 
 
-/**
- * @return {Object} Contains normalized x1, y1, x2 and y2 numbers.
+/*!
+ * @private
  */
-Bezierizer.prototype.getHandlePositions = function () {
+Bezierizer.prototype._updateInternalStateFromDOM = function () {
   var $canvasContainer = this._$canvasContainer;
 
-  return {
+  this._points = {
      x1: getHandleX($canvasContainer, this._$handles.eq(0))
     ,y1: getHandleY($canvasContainer, this._$handles.eq(0))
     ,x2: getHandleX($canvasContainer, this._$handles.eq(1))
@@ -74,14 +90,37 @@ Bezierizer.prototype.getHandlePositions = function () {
 };
 
 
+/**
+ * @return {Object} Contains normalized x1, y1, x2 and y2 numbers.
+ */
+Bezierizer.prototype.getHandlePositions = function () {
+  return $.extend({}, this._points);
+};
+
+
+/**
+ * @param {Object} points An object that may contain numbers representing any or all of x1, y1, x2, and y2.
+ */
+Bezierizer.prototype.setHandlePositions = function (points) {
+  $.extend(this._points, points);
+
+  this._$handles.eq(0).css('left', this._points.x1);
+  this._$handles.eq(0).css('top',  this._points.y1);
+  this._$handles.eq(1).css('left', this._points.x2);
+  this._$handles.eq(1).css('top',  this._points.y2);
+
+  this.redraw();
+};
+
+
 Bezierizer.prototype.redraw = function () {
-  var handlePositions = this.getHandlePositions();
+  var points = this._points;
   var height = this._$canvas[0].height;
   var width = this._$canvas[0].width;
-  var x1 = handlePositions.x1 * width;
-  var y1 = handlePositions.y1 * height;
-  var x2 = handlePositions.x2 * width;
-  var y2 = handlePositions.y2 * height;
+  var x1 = points.x1 * width;
+  var y1 = points.y1 * height;
+  var x2 = points.x2 * width;
+  var y2 = points.y2 * height;
 
   var ctx = this._ctx;
   ctx.clearRect(0, 0, width, height);
