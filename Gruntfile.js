7ead5b69477ffa52f98d9fa40e4c0b1767967eb7
Jeremy Kahn <jeremyckahn@gmail.com>
2013-09-16 21:33:59 -0500

    Adds some library boilerplate.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+/*global module:false, require:true, console:true */
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-dox');
+
+  var banner = [
+        '/*! <%= pkg.name %> - v<%= pkg.version %> - ',
+        '<%= grunt.template.today("yyyy-mm-dd") %> - <%= pkg.homepage %> */\n'
+      ].join('');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      standardTarget: {
+        options: {
+          banner: banner
+        },
+        src: [
+          'src/bezierizer.intro.js',
+          'src/bezierizer.template.js',
+          'src/bezierizer.core.js',
+          'src/bezierizer.outro.js'
+        ],
+        dest: 'dist/bezierizer.js'
+      }
+    },
+    uglify: {
+      standardTarget: {
+        files: {
+          'dist/bezierizer.min.js': ['dist/bezierizer.js']
+        }
+      },
+      options: {
+        banner: banner
+      }
+    },
+    jshint: {
+      all_files: [
+        'grunt.js',
+        'src/!(intro|outro)*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    dox: {
+      options: {
+        title: 'Bezierizer'
+      },
+      files: {
+        src: [
+          'src/init.js'
+        ],
+        dest: 'dist/doc'
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit']);
+  grunt.registerTask('build', [
+      'concat:standardTarget',
+      'uglify:standardTarget',
+      'dox']);
+
+};
