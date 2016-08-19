abc9af4e8da8177e6577d3b511ce362e6ed8c573
Evan Bowling <evan.j.bowling@gmail.com>
2016-08-18 22:54:11 -0500

    Update index.html to use requirejs and load lib as AMD.

---
+++ app.js
@@ -0,0 +1,16 @@
+// require.js config file for demo index.html
+
+requirejs.config({
+    "baseUrl": "",
+    "paths": {
+      "bezierizer": "dist/bezierizer",
+      "jquery": "dist/jquery",
+      "jquery-dragon": "dist/jquery.dragon",
+      "shifty":"dist/shifty"
+    },
+    "shim": {
+        "jquery-dragon": ["jquery"]
+    }
+});
+
+requirejs(["main"]);
